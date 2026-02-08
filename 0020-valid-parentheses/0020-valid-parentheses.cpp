6d2752298db4a5bb50c4cd0123aa07cddca1f923
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-08 14:28:43 +0530

    Time: 3 ms (9.43%), Space: 8.6 MB (97.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            char ch = s[i];
+        if(ch == '(' || ch == '{' || ch == '['){
+            st.push(ch);
+        }else{
+            if(st.empty()) return false;
+            char top = st.top();
+            st.pop(); 
+        if((ch == ')' && top != '(')||
+        (ch == '}' && top != '{') ||
+         (ch == ']' && top != '[')){
+          return false;
+         }
+        }
+        }
+         return st.empty();
+    }
+};
