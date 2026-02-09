349cc04d9cf19888eb17168dd881215b9c979523
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-09 19:08:28 +0530

    Time: 24 ms (48.25%), Space: 102.8 MB (82.33%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,27 +1,58 @@
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         vector<int> ans;
+//         stack<int>st;
+//         int n = temperatures.size();
+//         for(int i = n-1;i>=0;i--){
+//             if(st.empty()){
+//                 st.push(i);
+//                 ans.push_back(0);
+//             }else {
+//                 while(!st.empty()&& temperatures[st.top()]<=temperatures[i]){
+//                 st.pop();
+//                 }
+//                  if(st.empty()){
+//                 st.push(i);
+//                 ans.push_back(0);
+//                  }else{
+//                     ans.push_back(st.top()-i);
+//                     st.push(i);
+//                  }
+//             }
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> ans;
-        stack<int>st;
-        int n = temperatures.size();
-        for(int i = n-1;i>=0;i--){
-            if(st.empty()){
-                st.push(i);
-                ans.push_back(0);
-            }else {
-                while(!st.empty()&& temperatures[st.top()]<=temperatures[i]){
-                st.pop();
+        int n  = temperatures.size();
+        int maxi = INT_MIN;
+        vector<int>ans(n,0);
+        for(int i=n-1;i>=0;i--){
+            if(temperatures[i]>=maxi){
+                maxi = temperatures[i];
+            }else{
+                int j = i+1;
+                while(temperatures[j]<=temperatures[i]){
+                    j = j+ans[j];
                 }
-                 if(st.empty()){
-                st.push(i);
-                ans.push_back(0);
-                 }else{
-                    ans.push_back(st.top()-i);
-                    st.push(i);
-                 }
+                ans[i] = j-i;
             }
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        }return ans;
     }
 };
+
+
+
+
+
+
+
+
+
