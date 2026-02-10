a10f13565dcfc71372bf139f49a4a4c3a008e122
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-10 11:23:05 +0530

    Time: 1921 ms (51.16%), Space: 530.3 MB (46.75%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int ans = 0;
         for(int i=0;i<n;i++){
-            set<int>even,odd; //for i will increment make another set to compare
+            unordered_set<int>even,odd; //for i will increment make another set to compare
             for(int j=i;j<n;j++){
                 if(nums[j]%2==0){
                     even.insert(nums[j]);
