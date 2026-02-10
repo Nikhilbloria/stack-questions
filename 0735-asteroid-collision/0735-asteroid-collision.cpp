9f8309a35ae3e8908273254715b75f44679ce036
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-10 17:52:53 +0530

    Time: 0 ms (100%), Space: 22.2 MB (40.38%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -5,12 +5,12 @@ public:
         stack<int>st;
         for(int &a:nums){
             while(!st.empty() && a<0 && st.top()>0){
-                int sum = a+st.top();
+                int sum = a+st.top();///to check the top present in stack is big then a or not
                 if(sum<0){
                     st.pop();
                 }else if (sum>0){
-                    a=0;
-                }else{//sum==0
+                    a=0;//so that it can't be pushed in stack
+                }else{//sum==0;//means both no are equal and both will blast
                     st.pop();
                     a=0;
                 }
