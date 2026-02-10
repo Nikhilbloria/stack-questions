17bdeb5857aae98ab1580e414e755c606ba6e844
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-10 17:47:18 +0530

    Time: 0 ms (100%), Space: 22.1 MB (40.38%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,28 +3,23 @@ public:
     vector<int> asteroidCollision(vector<int>& nums) {
         int n = nums.size();
         stack<int>st;
-        
-        for(int i =0;i<n;i++){
-            int x = nums[i];
-            bool neg = false;
-            while(!st.empty() && st.top()>0 && x<0){
-                if(st.top()<-x){
+        for(int &a:nums){
+            while(!st.empty() && a<0 && st.top()>0){
+                int sum = a+st.top();
+                if(sum<0){
                     st.pop();
+                }else if (sum>0){
+                    a=0;
+                }else{//sum==0
+                    st.pop();
+                    a=0;
                 }
-            else if (st.top()==-x){
-                 st.pop();
-                 neg = true;
-                 break;
-            }else{
-                neg = true;
-                break;
+            }
+            if(a!=0){
+                st.push(a);
             }
         }
-        if(!neg){
-            st.push(x);
-        }
-    }
-        vector<int> ans(st.size());
+ vector<int> ans(st.size());
         for(int i = ans.size()-1;i>=0;i--){
             ans[i]=st.top();
             st.pop();
@@ -33,16 +28,3 @@ public:
     }
 };
 
-
-
-// // iterate 
-// if(i is negative and stack top < negative {
-//      while top is  empty &top is > negative {
-//         then pop
-//     }  
-//    } else{top is > negative)
-//     remove negative
-//     } 
-
-//     push i;
-// return st;
