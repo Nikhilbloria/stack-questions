1ef5558a7d45bf4b485c530e6853c57352a15598
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-10 12:21:59 +0530

    Time: 16 ms (68.51%), Space: 28.3 MB (74.49%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,24 +1,38 @@
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         int n = s.length();
+//         stack<char>st;
+//         for(int i=0;i<n;i++){
+//             if(s[i]=='*'){
+//                 st.pop();
+//                 continue;
+//             }else{
+//                 if(s[i]!='*'){
+//                 st.push(s[i]);
+//                 }
+//             }
+//         }
+//             string sto;
+//             while(!st.empty()){
+//                 sto.push_back(st.top());
+//                 st.pop();
+//             }
+//             reverse(sto.begin(),sto.end());
+//         return sto;
+//     }
+// };
+
 class Solution {
 public:
     string removeStars(string s) {
-        int n = s.length();
-        stack<char>st;
-        for(int i=0;i<n;i++){
+        string result;
+        for(int i =0;i<s.length();i++){
             if(s[i]=='*'){
-                st.pop();
-                continue;
+                result.pop_back();
             }else{
-                if(s[i]!='*'){
-                st.push(s[i]);
-                }
+                result.push_back(s[i]);
             }
-        }
-            string sto;
-            while(!st.empty()){
-                sto.push_back(st.top());
-                st.pop();
-            }
-            reverse(sto.begin(),sto.end());
-        return sto;
+        }return result;
     }
 };
