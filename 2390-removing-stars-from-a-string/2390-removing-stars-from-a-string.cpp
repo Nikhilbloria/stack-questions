e771f5717ec4d2a273f0eb7f29d2b50ef3a7b78a
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-02-10 12:26:32 +0530

    Time: 20 ms (46.99%), Space: 28.3 MB (74.49%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -23,16 +23,33 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         string result;
+//         for(int i =0;i<s.length();i++){
+//             if(s[i]=='*'){
+//                 result.pop_back();
+//             }else{
+//                 result.push_back(s[i]);
+//             }
+//         }return result;
+//     }
+// };
 class Solution {
 public:
     string removeStars(string s) {
-        string result;
-        for(int i =0;i<s.length();i++){
-            if(s[i]=='*'){
+
+        string result = "";
+
+        for (char c : s) {
+            if (c == '*') {
                 result.pop_back();
-            }else{
-                result.push_back(s[i]);
+            } else {
+                result.push_back(c);
             }
-        }return result;
+        }
+
+        return result;
     }
 };
